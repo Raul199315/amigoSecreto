1195dd2c734ac25a1d7e34e16f35cb8e61dd24c8
Raul199315 <102283554+Raul199315@users.noreply.github.com>
2025-02-11 21:08:04 -0500

    amigo Secreto V.05
    se agrega funcion para capturar nombre y agregarlo a una lista

--- app.js
+++ app.js
@@ -2,4 +2,45 @@
 //  Aquí deberás desarrollar la lógica para resolver el problema.
 
 //lista de amigos
-let amigos = [Marias,Jose,Raul,Bibiana];
+let amigos = [];
+
+// funcio para agregar nombres a la lista
+
+function agregarAmigo(){
+    let nombre = document.getElementById('amigo').value;
+    if(nombre === ''){
+        //alerta de que no se a agregado texto al input
+        alert("Por favor, inserte un nombre.")
+    }else{
+        amigos.push(nombre)
+        limpiarInput()
+        actualizarLista()
+        console.log(amigos)
+        
+    }
+}
+
+// funcion limpiar input
+function limpiarInput(){
+    let nombre = document.getElementById('amigo').value = '';
+}
+
+
+/// funcion barrer la lista
+function actualizarLista() {
+    // Obtener el elemento de la lista
+    let lista = document.getElementById('listaAmigos');
+    
+    // Limpiar la lista existente
+    lista.innerHTML = '';
+
+    // Iterar sobre el arreglo de amigos
+    for (let i = 0; i < amigos.length; i++) {
+        // Crear un nuevo elemento de lista
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        
+        // Agregar el nuevo elemento a la lista
+        lista.appendChild(li);
+    }
+}
