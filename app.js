42c98f63627418df52e6c8946ab864ffcca88315
Raul199315 <102283554+Raul199315@users.noreply.github.com>
2025-02-11 20:15:57 -0500

    amigo secreto v0.1
    creando repositorio en git hub

---
+++ app.js
@@ -0,0 +1,5 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación.
+//  Aquí deberás desarrollar la lógica para resolver el problema.
+
+//lista de amigos
+let amigos = [Marias,Jose,Raul,Bibiana];
